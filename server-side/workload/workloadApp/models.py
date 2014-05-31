6ab9becfa390987ff904f0a932ecda2972e02324
kon <schubert.konstantin@gmail.com>
2014-05-31 17:06:26 +0200

    working on calendar page

--- server-side/workload/workloadApp/models.py
+++ server-side/workload/workloadApp/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class Lecture(models.Model):
     semester = models.CharField(max_length=6) #e.g. WS2014
     name = models.CharField(max_length=200)
-    startweek = models.DateField()
-    endweek = models.DateField()
+    startDay = models.DateField() # The day of the first lecture event. Or the monday of the week when the lecture starts.
+    endDay = models.DateField()   # The day of the last lecture event. Or the day of the exam. Something like this.
 
 
 # The Student model is going to be written as an extension of the user model once the shibboleth login has been implemented
@@ -15,6 +15,16 @@ class Student(models.Model):
     lectures = models.ManyToManyField(Lecture,blank=True)
     user = models.OneToOneField(User)
 
+    def startOfLectures(self):
+        if not self.lectures:
+            raise Exception("no lectures found")
+        min(self.lectures.all().startDay)
+
+    def endOfLectures(self):
+        if not self.lectures:
+            raise Exception("no lectures found")
+        min(self.lectures.all().endDay)
+
 
 class WorkingHoursEntry(models.Model):
     hoursInLecture=models.IntegerField(default=0)
@@ -22,7 +32,7 @@ class WorkingHoursEntry(models.Model):
     hoursStudying=models.IntegerField(default=0)
     lecture = models.ForeignKey(Lecture)
     student = models.ForeignKey(Student)
-    week = models.DateField()
+    week = models.DateField() # The Monday of the week for which the working hours are entered
 
     def __unicode__(self):  # Python 3: def __str__(self):
         return "working hours for student" + student.permanentId + "in week number" + week.isocalendar()
--- server-side/workload/workloadApp/views.py
+++ server-side/workload/workloadApp/views.py
@@ -6,8 +6,12 @@ from django.template import RequestContext, loader
 
 def calendar(request):
 
-    if not request.user.is_authenticated():
+    if not request.user.is_authenticated(): # If the user is not logged in, make him log in :)
         return HttpResponse("Not logged in. TODO: Redirect to login page")
+
+    if not request.user.lectures:   # If the user 
+        return HttpResponse("No lectures chosen. TODO: Redirect to page where lectures can be chosen")
+
     else:
         template = loader.get_template('workloadApp/calendar.html')
         context = RequestContext(request, {
