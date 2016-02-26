1a00395de9cb7e8e0d413391aab2137983508391
Konstantin Schubert <schubert.konstantin@gmail.com>
2016-02-26 20:27:50 +0100

    update admin view, I think this fixes #55

--- server-side/workload/workloadApp/admin.py
+++ server-side/workload/workloadApp/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from workloadApp.models import Lecture, Student, WorkingHoursEntry
 
 # Register your models here.
-admin.site.register(Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('name', 'semester')
+    ordering = ["-startDay"]
+admin.site.register(Lecture, LectureAdmin)
 admin.site.register(Student)
 admin.site.register(WorkingHoursEntry)
